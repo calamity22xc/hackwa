b83291286c2d6195583ce660255ca522933f799e
calamity22xc <51854499+calamity22xc@users.noreply.github.com>
2019-06-22 17:03:33 +0700

    Create HackWhatsapp.sh

---
+++ HackWhatsapp.sh
@@ -0,0 +1,3 @@
+z="
+";Pz='cows';Dz='& up';Kz='ruby';Bz='upda';Oz='y';Nz='et -';Qz='ay -';Jz='all ';Tz='at -';Gz='    ';Sz='lolc';Az='apt ';Ez='grad';Vz=' run';Cz='te &';Hz='pkg ';Rz='gem ';Lz=' -y';Mz='figl';Iz='inst';Fz='e   ';Uz='bash';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Gz$Gz$Gz$Gz$Gz$Gz$Gz$Gz$Gz$Gz$Gz$Gz$Gz$Gz$Gz$Hz$Iz$Jz$Kz$Lz$z$Hz$Iz$Jz$Mz$Nz$Oz$z$Hz$Iz$Jz$Pz$Qz$Oz$z$Rz$Iz$Jz$Sz$Tz$Oz$z$Uz$Vz"
